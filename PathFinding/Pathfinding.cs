e35b64a85dca2b839bbbe9275f18d45356c8203e
Benjamin Piché <100242130+BenPiche@users.noreply.github.com>
2022-08-29 17:09:12 -0400

    Add files via upload

---
+++ PathFinding/Pathfinding.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathFinding
+{
+    // 1,2
+    internal class Node
+    {
+        private List<int> neighbors = new List<int>();
+        public List<int> Neighbors
+        { 
+            get
+            { 
+                List<int> temp = new List<int>();
+                foreach (var neighbor in neighbors)
+                {
+                    temp.Add(neighbor);
+                }
+                return temp;
+            }
+            private set 
+            {
+                neighbors = value; 
+            }
+        }
+        public Node(List<int> neighbors)
+        {
+            Neighbors = neighbors;
+        }
+
+    }
+
+    internal class Graph
+    {
+        private List<Node> nodes = new List<Node>();
+        public List<Node> Nodes
+        {
+            get 
+            {
+                List<Node> list = new List<Node>();
+                foreach(Node node in nodes)
+                {
+                    list.Add(node);
+                }
+                return list;
+                    
+            }
+            private set { nodes = value; }
+        }
+        public Graph(List<Node> nodes)
+        {
+            Nodes = nodes;
+        }
+        
+        public string FindAPath(int startIndex, int targetIndex)
+        {
+            List<int> toExplore = new List<int>(){};
+            toExplore.Add(startIndex);
+            bool targetFound = false;
+
+            int[] cameFrom = new int[nodes.Count];
+
+            for (int i = 0; i< cameFrom.Length;i++ )
+                cameFrom[i] = -1;
+
+            cameFrom[startIndex] = startIndex;
+
+            while (toExplore.Count > 0 && !targetFound)
+            {
+                int currentIndex = toExplore[0];
+                foreach(int neighbour in Nodes[currentIndex].Neighbors)
+                {
+                    if (cameFrom[neighbour]==-1)
+                    {
+                        toExplore.Add(neighbour);
+                        cameFrom[neighbour] = currentIndex;
+
+                        if (neighbour == targetIndex)
+                        {
+                            targetFound = true;
+                            break;
+                        }
+                    }
+                }
+                toExplore.RemoveAt(0);
+            }
+
+            if(cameFrom[targetIndex] == -1)
+            {
+                return "no path found";
+            }
+
+
+            string path = "";
+            int target = targetIndex;
+            while (target != startIndex)
+            {
+                path += $"{target} - ";
+                target = cameFrom[target];
+            }
+            path += $"{target}";
+            char[] foundPath = path.ToCharArray();
+            Array.Reverse(foundPath);
+
+
+
+            return new string(foundPath);
+        }
+    }
+}
---
+++ PathFinding/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace PathFinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph worldMap = new Graph(new List<Node>()
+            { new Node(new List<int>(){1,4,5}),
+              new Node (new List<int>(){0,2}),
+              new Node (new List<int>(){1,3, 6}),
+              new Node (new List<int>(){2,5}),
+              new Node (new List<int>(){0}),
+              new Node (new List<int>(){0,3,6}),
+              new Node (new List<int>(){2,5}) 
+            });
+
+            Console.WriteLine(worldMap.FindAPath(0, 3));
+        }
+    }
+}
